7ca5d5bbe17018376b865ab82d32ff1e93ecc6c3
Adrian <adriandinca2008@yahoo.com>
2019-05-14 11:50:51 +0300

    feat: Add svg support

--- src/components/image.js
+++ src/components/image.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { memo, useEffect } from 'react';
 import { StaticQuery, graphql } from 'gatsby';
 import Img from 'gatsby-image';
+import { useInView } from 'react-intersection-observer';
 
 /*
  * This component is built using `gatsby-image` to automatically serve optimized
@@ -13,20 +14,67 @@ import Img from 'gatsby-image';
  * - `StaticQuery`: https://gatsby.dev/staticquery
  */
 
-const Image = () => (
+const LazySvg = memo(({ src, otherProps }) => {
+  const [ref, inView] = useInView({
+    rootMargin: '200px',
+    triggerOnce: true,
+  });
+
+  useEffect(() => {
+    const preloadLink = document.createElement('link');
+    preloadLink.href = src;
+    preloadLink.rel = 'preload';
+    preloadLink.as = 'image';
+    document.head.appendChild(preloadLink);
+  }, []);
+
+  return <img src={inView ? src : ''} ref={ref} {...otherProps} />;
+});
+
+const Image = memo(({ src, ...otherProps }) => (
   <StaticQuery
     query={graphql`
       query {
-        placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
-          childImageSharp {
-            fluid(maxWidth: 300) {
-              ...GatsbyImageSharpFluid
+        images: allFile {
+          edges {
+            node {
+              relativePath
+              name
+              extension
+              publicURL
+              childImageSharp {
+                fluid(maxWidth: 665) {
+                  ...GatsbyImageSharpFluid_withWebp
+                }
+              }
             }
           }
         }
       }
     `}
-    render={data => <Img fluid={data.placeholderImage.childImageSharp.fluid} />}
+    render={({ images }) => {
+      const srcSplits = src.split('/images/');
+
+      if (srcSplits[1] === undefined) {
+        throw new Error(
+          `"${src}" does not resolve to the "/src/images/" folder`
+        );
+      }
+
+      src = srcSplits[1];
+
+      const image = images.edges.find(({ node }) => node.relativePath === src);
+
+      // TODO: Check for external urls or url encodings
+      if (!image) { return <img src={src} {...otherProps} />; }
+
+      if (image.node.extension === 'svg') {
+        return <LazySvg src={image.node.publicURL} {...otherProps} />;
+      }
+
+      return <Img fluid={image.node.childImageSharp.fluid} {...otherProps} />;
+    }}
   />
-);
+));
+
 export default Image;
