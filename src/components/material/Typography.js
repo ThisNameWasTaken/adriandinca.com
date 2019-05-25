80f81693fd4b351182e4c7f21f2845e52789724a
Adrian <adriandinca2008@yahoo.com>
2019-05-25 21:58:09 +0300

    feat: Add material typography

---
+++ src/components/material/Typography.js
@@ -0,0 +1,17 @@
+import '@material/react-typography/index.scss';
+
+export {
+  Body1,
+  Body2,
+  Button,
+  Caption,
+  Headline1,
+  Headline2,
+  Headline3,
+  Headline4,
+  Headline5,
+  Headline6,
+  Overline,
+  Subtitle1,
+  Subtitle2,
+} from '@material/react-typography';
