bef2cd209cb7e72a7cd437e76e95d0567202dbbf
Adrian <adriandinca2008@yahoo.com>
2019-06-09 21:07:15 +0300

    feat: Add material card

---
+++ src/components/material/Card.js
@@ -0,0 +1,18 @@
+import Card, {
+  CardPrimaryContent,
+  CardMedia,
+  CardActions,
+  CardActionButtons,
+  CardActionIcons,
+} from '@material/react-card';
+import './Card.scss';
+
+export default Card;
+
+export {
+  CardPrimaryContent,
+  CardMedia,
+  CardActions,
+  CardActionButtons,
+  CardActionIcons,
+};
