3aa8ed412340150ec4a263034b8f7e1cde16fa5d
Adrian <adriandinca2008@yahoo.com>
2019-06-09 21:08:59 +0300

    feat: Add project card

---
+++ src/components/ProjectCard.js
@@ -0,0 +1,48 @@
+import React, { memo } from 'react';
+import Card, {
+  CardPrimaryContent,
+  CardMedia,
+  CardActions,
+  CardActionButtons,
+} from './material/Card';
+import { Headline4, Body1 } from './material/Typography';
+import Button from './material/Button';
+import Image from './image';
+import { ChipSet, Chip } from './material/Chip';
+
+const ProjectCard = ({
+  headline,
+  body,
+  media,
+  sourceButton,
+  demoButton,
+  tags,
+  className = '',
+  ...otherProps
+}) => (
+  <Card className={`project-card ${className}`} {...otherProps}>
+    <CardPrimaryContent onClick={() => (window.location = demoButton.href)}>
+      <CardMedia wide>
+        <div className="project-card__media-image">
+          <Image {...media} />
+        </div>
+        <Headline4 className="project-card__headline">{headline}</Headline4>
+      </CardMedia>
+      <Body1 className="project-card__body">{body}</Body1>
+    </CardPrimaryContent>
+
+    <ChipSet>
+      {tags.map(tag => (
+        <Chip label={tag} tabIndex={-1} />
+      ))}
+    </ChipSet>
+    <CardActions>
+      <CardActionButtons>
+        <Button href={demoButton.href}>{demoButton.label}</Button>
+        <Button href={sourceButton.href}>{sourceButton.label}</Button>
+      </CardActionButtons>
+    </CardActions>
+  </Card>
+);
+
+export default memo(ProjectCard);
