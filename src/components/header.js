efc2c30fef2dc8127049d891216704c722b51395
Adrian <adriandinca2008@yahoo.com>
2019-06-09 22:43:15 +0300

    feat: Add top app bar

--- src/components/header.js
+++ src/components/header.js
@@ -1,36 +1,63 @@
-import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
 import React from 'react';
+import TopAppBar, {
+  TopAppBarRow,
+  TopAppBarSection,
+  TopAppBarIcon,
+  TopAppBarTitle,
+} from './material/TopAppBar';
+import MaterialIcon from './material/MaterialIcon';
+
+import IconButton from './material/IconButton';
+import Button from './material/Button';
+
+// @ts-ignore
+// import MenuSvg from '../images/inline/baseline-menu-24px.svg';
+// @ts-ignore
+import WorkSvg from '../images/inline/baseline-work-24px.svg';
+import PersonSvg from '../images/inline/baseline-person-24px.svg';
+import MailSvg from '../images/inline/baseline-mail-24px.svg';
+import GithubSvg from '../images/inline/github-brands.svg';
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-      position: 'fixed',
-      top: 0,
-      width: '100%',
-      display: 'block',
-    }}
-  >
-    <div
-      style={{
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+  <TopAppBar>
+    <TopAppBarRow>
+      <TopAppBarSection align="start">
+        {/* <TopAppBarIcon navIcon tabIndex={0}>
+          <IconButton>
+            <MenuSvg />
+          </IconButton>
+        </TopAppBarIcon> */}
+        <TopAppBarTitle>
+          <Button href="/" className="site-title">
+            {siteTitle}
+          </Button>
+        </TopAppBarTitle>
+      </TopAppBarSection>
+      <TopAppBarSection align="end" role="toolbar">
+        <TopAppBarIcon actionItem tabIndex={0}>
+          <IconButton isLink href="#projects">
+            <WorkSvg />
+          </IconButton>
+        </TopAppBarIcon>
+        <TopAppBarIcon actionItem tabIndex={0}>
+          <IconButton isLink href="#about">
+            <PersonSvg />
+          </IconButton>
+        </TopAppBarIcon>
+        <TopAppBarIcon actionItem tabIndex={0}>
+          <IconButton isLink href="#contact">
+            <MailSvg />
+          </IconButton>
+        </TopAppBarIcon>
+        <TopAppBarIcon actionItem tabIndex={0}>
+          <IconButton isLink href="https://github.com/ThisNameWasTaken/">
+            <GithubSvg />
+          </IconButton>
+        </TopAppBarIcon>
+      </TopAppBarSection>
+    </TopAppBarRow>
+  </TopAppBar>
 );
 
 Header.propTypes = {
