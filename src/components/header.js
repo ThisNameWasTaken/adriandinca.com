1b538f342807b594de2ca3cf2bb96e1c716e04ef
Adrian <adriandinca2008@yahoo.com>
2019-06-11 20:07:39 +0300

    feat: Label app bar icons properly

--- src/components/header.js
+++ src/components/header.js
@@ -6,10 +6,10 @@ import TopAppBar, {
   TopAppBarIcon,
   TopAppBarTitle,
 } from './material/TopAppBar';
-import MaterialIcon from './material/MaterialIcon';
 
 import IconButton from './material/IconButton';
 import Button from './material/Button';
+import Tooltip from './material/Tooltip/Tooltip';
 
 // @ts-ignore
 // import MenuSvg from '../images/inline/baseline-menu-24px.svg';
@@ -20,44 +20,70 @@ import MailSvg from '../images/inline/baseline-mail-24px.svg';
 import GithubSvg from '../images/inline/github-brands.svg';
 
 const Header = ({ siteTitle }) => (
-  <TopAppBar>
-    <TopAppBarRow>
-      <TopAppBarSection align="start">
-        {/* <TopAppBarIcon navIcon tabIndex={0}>
+  <>
+    <TopAppBar>
+      <TopAppBarRow>
+        <TopAppBarSection align="start">
+          {/* <TopAppBarIcon navIcon tabIndex={0}>
           <IconButton>
             <MenuSvg />
           </IconButton>
         </TopAppBarIcon> */}
-        <TopAppBarTitle>
-          <Button href="/" className="site-title">
-            {siteTitle}
-          </Button>
-        </TopAppBarTitle>
-      </TopAppBarSection>
-      <TopAppBarSection align="end" role="toolbar">
-        <TopAppBarIcon actionItem tabIndex={0}>
-          <IconButton isLink href="#projects">
-            <WorkSvg />
-          </IconButton>
-        </TopAppBarIcon>
-        <TopAppBarIcon actionItem tabIndex={0}>
-          <IconButton isLink href="#about">
-            <PersonSvg />
-          </IconButton>
-        </TopAppBarIcon>
-        <TopAppBarIcon actionItem tabIndex={0}>
-          <IconButton isLink href="#contact">
-            <MailSvg />
-          </IconButton>
-        </TopAppBarIcon>
-        <TopAppBarIcon actionItem tabIndex={0}>
-          <IconButton isLink href="https://github.com/ThisNameWasTaken/">
-            <GithubSvg />
-          </IconButton>
-        </TopAppBarIcon>
-      </TopAppBarSection>
-    </TopAppBarRow>
-  </TopAppBar>
+          <TopAppBarTitle>
+            <Button href="/" className="site-title">
+              {siteTitle}
+            </Button>
+          </TopAppBarTitle>
+        </TopAppBarSection>
+        <TopAppBarSection align="end" role="toolbar">
+          <TopAppBarIcon actionItem tabIndex={0}>
+            <IconButton
+              isLink
+              href="#projects"
+              aria-label="view projects"
+              id="projects-top-app-bar-link"
+            >
+              <WorkSvg aria-hidden="true" />
+            </IconButton>
+          </TopAppBarIcon>
+          <TopAppBarIcon actionItem tabIndex={0}>
+            <IconButton
+              isLink
+              href="#about"
+              aria-label="read more about me"
+              id="about-top-app-bar-link"
+            >
+              <PersonSvg aria-hidden="true" />
+            </IconButton>
+          </TopAppBarIcon>
+          <TopAppBarIcon actionItem tabIndex={0}>
+            <IconButton
+              isLink
+              href="#contact"
+              aria-label="get contact details"
+              id="contact-top-app-bar-link"
+            >
+              <MailSvg aria-hidden="true" />
+            </IconButton>
+          </TopAppBarIcon>
+          <TopAppBarIcon actionItem tabIndex={0}>
+            <IconButton
+              isLink
+              href="https://github.com/ThisNameWasTaken/"
+              aria-label="visit github profile"
+              id="github-top-app-bar-link"
+            >
+              <GithubSvg aria-hidden="true" />
+            </IconButton>
+          </TopAppBarIcon>
+        </TopAppBarSection>
+      </TopAppBarRow>
+    </TopAppBar>
+    <Tooltip htmlFor="projects-top-app-bar-link">Projects</Tooltip>
+    <Tooltip htmlFor="about-top-app-bar-link">About</Tooltip>
+    <Tooltip htmlFor="contact-top-app-bar-link">Contact</Tooltip>
+    <Tooltip htmlFor="github-top-app-bar-link">Github</Tooltip>
+  </>
 );
 
 Header.propTypes = {
