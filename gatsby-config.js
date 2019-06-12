5c47c611b007842282d5dad685207fa8bbf0afee
Adrian <adriandinca2008@yahoo.com>
2019-06-12 23:26:53 +0300

    fix: Styles breakage

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,17 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Adrian Dinca`,
+    description: `A website for those looking for maintaining, updatinig, optimising or creating a website. Available services include progressive web app conversion, SEO (search engine optimisation), website auditing and more.`,
+    author: `@ThisNameWasTaken`,
   },
   plugins: [
     {
       resolve: 'gatsby-plugin-react-svg',
-      options: {
-        rule: {
-          include: /inline/,
-        },
-      },
+      // options: {
+      //   rule: {
+      //     include: /inline/,
+      //   },
+      // },
     },
     {
       resolve: `gatsby-plugin-sass`,
@@ -24,6 +24,7 @@ module.exports = {
               {
                 autoprefixer: false,
                 zindex: false,
+                reduceIdents: false,
                 discardComments: {
                   removeAll: true,
                 },
@@ -37,9 +38,10 @@ module.exports = {
       resolve: `gatsby-plugin-purgecss`,
       options: {
         printRejected: true, // Print removed selectors and processed file names
-        // develop: true, // Enable while using `gatsby develop`
+        develop: true, // Enable while using `gatsby develop`
         // tailwind: true, // Enable tailwindcss support
         // whitelist: ['whitelist'], // Don't remove this selector
+        whitelistPatterns: [/mdc-/, /upgraded/, /gatsby/], // Don't remove this selector
         // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
         // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
       },
