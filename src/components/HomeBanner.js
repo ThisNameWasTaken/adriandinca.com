77d1e8d76d61c78686ad337783ab420647bbc835
Adrian <adriandinca2008@yahoo.com>
2019-07-09 21:08:01 +0300

    feat: Lazy load the active svg

--- src/components/HomeBanner.js
+++ src/components/HomeBanner.js
@@ -1,15 +1,21 @@
-import React, { useRef, useEffect } from 'react';
+import React, { Suspense, lazy } from 'react';
 import './HomeBanner.scss';
 
-import SvgActive from '../images/home-banner-active.svg';
 import SvgIdle from '../images/home-banner-idle.svg';
 import SvgIdlePlants from '../images/home-banner-idle-plants.svg';
+
+const SvgActive = lazy(() => import('../images/home-banner-active.svg'));
 
 const HomeBanner = () => {
   return (
     <div className="home-banner">
       <SvgIdle className="home-banner-svg home-banner-svg--idle" />
-      <SvgActive className="home-banner-svg home-banner-svg--active" />
+
+      {typeof window !== 'undefined' && (
+        <Suspense fallback={<svg viewBox="0 0 658.95 512.4" />}>
+          <SvgActive className="home-banner-svg home-banner-svg--active" />
+        </Suspense>
+      )}
       <SvgIdlePlants className="home-banner-svg home-banner-svg--idle" />
     </div>
   );
