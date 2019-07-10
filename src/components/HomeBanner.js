a3cbb7c3ca3fc490a2a6def6fb06546d4c14239f
Adrian <adriandinca2008@yahoo.com>
2019-07-10 14:29:35 +0300

    feat: Add responsive text

--- src/components/HomeBanner.js
+++ src/components/HomeBanner.js
@@ -10,10 +10,19 @@ const SvgActive = lazy(() => import('../images/home-banner-active.svg'));
 const HomeBanner = () => {
   return (
     <div className="home-banner">
-      <div className="home-banner__content" style={{ background: '#f43' }}>
-        {/*  */}
+      <div className="home-banner__content">
+        <h1 className="home-banner__headline">
+          <div>Web Development</div>
+          <div>& Maintenance</div>
+        </h1>
+
+        <div>
+          <Button raised href="mailto:adriandinca2008@gmail.com">
+            get in touch
+          </Button>
+        </div>
       </div>
-      <div className="home-banner__svgs" style={{ background: '#34f' }}>
+      <div className="home-banner__svgs">
         <SvgIdle className="home-banner__svg home-banner__svg--idle" />
         {typeof window !== 'undefined' && (
           <Suspense fallback={<svg viewBox="0 0 658.95 512.4" />}>
