9ba0e6b938bdcc5e7d9a7fd962024cb740ed52b6
Adrian <adriandinca2008@yahoo.com>
2019-07-13 16:46:20 +0300

    feat: Add CNAME script

---
+++ scripts/cname.js
@@ -0,0 +1,20 @@
+const { promisify } = require('util');
+const { readFile: _readFile, writeFile: _writeFile } = require('fs');
+
+const writeFile = promisify(_writeFile);
+const readFile = promisify(_readFile);
+
+(async () => {
+  const { homepage } = JSON.parse(await readFile('package.json', 'utf-8'));
+
+  if (!homepage) {
+    console.warn(
+      'Cannot create CNAME because package.json does not have a `homepage` property.'
+    );
+  }
+
+  const CNAME = homepage.split('://')[1];
+  await writeFile('public/CNAME', CNAME);
+
+  console.log(`Created CNAME ${CNAME}`);
+})();
