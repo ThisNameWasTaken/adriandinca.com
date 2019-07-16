03e7f47ee1ccda857165dce5fc7777b0c77d64ca
Adrian <adriandinca2008@yahoo.com>
2019-07-16 14:28:25 +0300

    chore: Update PWA manifest to match website's theme

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,13 +53,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Adrian Dinca`,
+        short_name: `Adrian Dinca`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        background_color: `#8d0ffa`,
+        theme_color: `#8d0ffa`,
+        display: `standalone`,
+        icon: `src/images/icons/website-logo.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
