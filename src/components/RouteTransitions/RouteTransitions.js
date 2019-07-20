f21f60e28ac0c1426f8d190c4659452b3c6fe5a1
Adrian <adriandinca2008@yahoo.com>
2019-07-20 20:07:42 +0300

    refactor: Store css vars based on class names

--- src/components/RouteTransitions/RouteTransitions.js
+++ src/components/RouteTransitions/RouteTransitions.js
@@ -5,24 +5,28 @@ Object.keys(sassVars).forEach(key => (sassVars[key] = parseInt(sassVars[key])));
 
 const classNameBase = `route-transition`;
 
+const classNames = {
+  base: classNameBase,
+  appear: `${classNameBase}-appear`,
+  appearActive: `${classNameBase}-appear-active`,
+  appearDone: `${classNameBase}-appear-active-appear-done`,
+  enter: `${classNameBase}-enter`,
+  enterActive: `${classNameBase}-enter-active`,
+  enterDone: `${classNameBase}-enter-done`,
+  exit: `${classNameBase}-exit`,
+  exitActive: `${classNameBase}-exit-active`,
+  exitDone: `${classNameBase}-exit-done`,
+};
+
+const cssVars = {
+  enterTranslateX: `--${classNames.enter}-translate-x`,
+  exitTranslateX: `--${classNames.exit}-translate-x`,
+};
+
 const routeTransitions = {
   sassVars,
-  cssVars: {
-    enterTranslateX: `--${classNameBase}-enter-translate-x`,
-    exitTranslateX: `--${classNameBase}-exit-translate-x`,
-  },
-  classNames: {
-    base: classNameBase,
-    appear: `${classNameBase}-appear`,
-    appearActive: `${classNameBase}-appear-active`,
-    appearDone: `${classNameBase}-appear-active-appear-done`,
-    enter: `${classNameBase}-enter`,
-    enterActive: `${classNameBase}-enter-active`,
-    enterDone: `${classNameBase}-enter-done`,
-    exit: `${classNameBase}-exit`,
-    exitActive: `${classNameBase}-exit-active`,
-    exitDone: `${classNameBase}-exit-done`,
-  },
+  cssVars,
+  classNames,
   moveToLeft() {
     document.documentElement.style.setProperty(
       this.cssVars.enterTranslateX,
